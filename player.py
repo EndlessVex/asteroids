9834b7365409872ef07927eeef11733eaa560139
EndlessVex <EndlessVexation@gmail.com>
2025-06-30 13:16:15 -0400

    added a shot timer to shooting mechanic

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0.0
     
     # triangle is from boot.dev
     def triangle(self):
@@ -27,12 +28,15 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        bullet.velocity = forward * PLAYER_SHOOT_SPEED
+        if self.shot_timer <= 0:
+            self.shot_timer = PLAYER_SHOOT_CONSTANT
+            bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            bullet.velocity = forward * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_timer -= dt
 
         if keys[pygame.K_w]:
             # right
