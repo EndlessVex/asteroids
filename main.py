d7c79f9d4588e0d4d3bfadcbb2bd792252eee03c
EndlessVex <EndlessVexation@gmail.com>
2025-06-30 13:21:09 -0400

    shots now destroy asteroids

--- main.py
+++ main.py
@@ -41,6 +41,11 @@ def main():
             if asteroid.collide(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shot_group:
+                if asteroid.collide(shot):
+                    shot.kill()
+                    asteroid.kill()
+                    
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
         #print(dt)
