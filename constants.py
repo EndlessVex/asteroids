4d5e6c838e0f4e13cad6f1aaaf3faaa2395dca2c
EndlessVex <EndlessVexation@gmail.com>
2025-06-30 10:44:42 -0400

    added the player.py and Player class, creating instance of player and drawing player in game loop

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
+# Screen Stuff
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+#Player Stuff
+PLAYER_RADIUS = 20
+
+# Asteroid Stuff
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     game_start = 1
@@ -10,13 +11,14 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     # Game Loop
     while game_start == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
         print(dt)
