4ba891ae4e252140acc5ca8b879b3fe652b823ec
EndlessVex <EndlessVexation@gmail.com>
2025-06-30 09:39:06 -0400

    limiting framerate to 60fps and updating the .fill to reference our screen instead of the surface

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     game_start = 1
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -14,8 +16,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
+        screen.fill(color="black")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
